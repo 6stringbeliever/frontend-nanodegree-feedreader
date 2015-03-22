90e5852ea072c6a2561878701837c70d28d4693a
Shaun Kelly <shaunkelly@gmail.com>
2015-03-22 13:04:18 -0400

    Fix spaces; add suite for new feed dialog; add test for updating header

--- README.md
+++ README.md
@@ -4,14 +4,13 @@ In this project you are given a web-based application that reads RSS feeds. The 
 
 # Additional Tests
 
-* In the "RSS Feeds" suite, add a test to make sure that the URL values in allFeeds
-are actually URLs
-* In the "The Menu" suite, add a test to make sure that the menu closes when
-users select a feed from the menu.
-* In the "New Feed Selection" suite, add a test to make sure the page header changes
-when a new feed is selected.
+* In the "RSS Feeds" suite, add a test to make sure that the URL values in allFeeds are actually URLs. This should pass.
+* In the "The Menu" suite, add a test to make sure that the menu closes when users select a feed from the menu. This should pass.
+* In the "New Feed Selection" suite, add a test to make sure the page header changes when a new feed is selected. This should pass.
+* Add a new test suite to test for future feature of being able to add new feeds. A "Add New Feeds" button will appear below the list of feeds on the menu. Clicking the button will display a dialog box. Users will enter a feed name and feed URL in the dialog box and click an "Add Feed" button. If the feed name or URL are invalid, the dialog box will display an error message. If the feed name and URL are valid, the dialog box will disappear and the feed name will be added to the allFeeds variable and the list in the menu will be updated. These should all fail miserably like my 2015 March Madness bracket.
 
 # References
 
 * http://evanhahn.com/how-do-i-jasmine/
-* Regular Expressions Cookbook by Jan Goyvaerts and Steven Levithan (for the URL regex)
+* Regular Expressions Cookbook by Jan Goyvaerts and Steven Levithan
+(for the URL regex)
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -100,12 +100,12 @@ $(function() {
     describe('New Feed Selection', function() {
 
         var postTitle,
-			headerText;
+            headerText;
 
         beforeEach(function(done) {
             loadFeed(0, function() {
                 postTitle = $('.feed .entry h2').text();
-				headerText = $('h1.header-title').text();
+                headerText = $('h1.header-title').text();
                 loadFeed(1, function() {
                     done();
                 });
@@ -116,12 +116,95 @@ $(function() {
             expect($('.feed .entry h2').text()).not.toBe(postTitle);
             done();
         });
-		
-		it('updates the feed title', function(done) {
-			expect($('h1.header-title').text()).not.toBe(headerText);
-			done();
-		});
 
+        it('updates the feed title', function(done) {
+            expect($('h1.header-title').text()).not.toBe(headerText);
+            done();
+        });
+
+    });
+
+    describe('Add new feed dialog', function() {
+
+        beforeEach(function() {
+            /* Open the menu. Click the add new feed button. */
+            $('body').toggleClass('menu-hidden', false);
+            $('#menu-add-new-feed').click();
+        });
+
+        afterEach(function() {
+            /* Hide the menu. Hide the dialog. Clear the dialog fields.*/
+            $('body').toggleClass('menu-hidden', true);
+            $('#add-feed-dialog').toggleClass('dialog-hidden', true);
+            $('#new-feed-name').val('');
+            $('#new-feed-url').val('');
+        });
+
+        it('displays when "Add New Feed" is clicked', function() {
+            expect($('#add-feed-dialog').length).toBeGreaterThan(0);
+            expect($('#add-feed-dialog').hasClass('dialog-hidden')).toBe(false);
+        });
+
+        it('closes the menu when "Add New Feed" is clicked', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+
+        it('displays an error if user does not enter a name', function() {
+            var feedName = '';
+            var feedURL = 'http://daringfireball.net/feeds/main';
+
+            /* Fill in the dialog. Valid URL. No Name. Click the button. */
+            $('#new-feed-name').val(feedName);
+            $('#new-feed-url').val(feedURL);
+            $('#new-feed-submit').click();
+
+            /* Verify that there is an error message. */
+            expect($('#add-feed-dialog .error-message').length).toBeGreaterThan(0);
+        });
+
+        it('displays an error if user enters an invalid URL', function() {
+            var feedName = 'Legit Feed Name';
+            var feedURL = 'Not a legit URL dot com tilde php dot rss';
+
+            /* Fill in the dialog. Valid Name. Invalid URL. Click the button. */
+            $('#new-feed-name').val(feedName);
+            $('#new-feed-url').val(feedURL);
+            $('#new-feed-submit').click();
+
+            /* Verify that there is an error message. */
+            expect($('#add-feed-dialog .error-message').length).toBeGreaterThan(0);
+        });
+
+        it('closes when the user adds a valid feed', function() {
+            var feedName = 'Daring Fireball';
+            var feedURL = 'http://daringfireball.net/feeds/main';
+
+            /* Fill in the dialog. Valid Name. Valid URL. Click the button. */
+            $('#new-feed-name').val(feedName);
+            $('#new-feed-url').val(feedURL);
+            $('#new-feed-submit').click();
+
+            /* Verify that there is no error message and the window is hidden. */
+            expect($('#add-feed-dialog .error-message').length).toBe(0);
+            expect($('#add-feed-dialog').hasClass('dialog-hidden')).toBe(true);
+        });
+
+        it('adds a valid feed to the menu', function() {
+            var startingFeedNum = allFeeds.length;
+            var feedName = 'Daring Fireball';
+            var feedURL = 'http://daringfireball.net/feeds/main';
+
+            /* Fill in the dialog. Valid Name. Valid URL. Click the button. */
+            $('#new-feed-name').val(feedName);
+            $('#new-feed-url').val(feedURL);
+            $('#new-feed-submit').click();
+
+            /* Verify that we've added one feed to allFeeds and that we've
+                updated the feed list in the menu. */
+            expect(allFeeds.length).toBe(startingFeedNum + 1);
+            expect($('.feed-list li:contains(' + feedName + ')').length)
+                .toBeGreaterThan(0);
+        });
     });
 
 }());
